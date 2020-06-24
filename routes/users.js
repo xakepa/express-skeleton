afceb4649b87a00bc5b10a1d0dd1d0857d86126d
xakepa <plamen_ivanov@mail.com>
2020-06-24 18:45:40 +0300

    Register functionality added

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,24 @@ router.get('/register', (req, res) => {
   res.render('./guest/register');
 })
 
-// router.post('/register', (req, res) => {
-
-// })
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    await Users.create({ email, password });
+  } catch (error) {
+    if (error) {
+      throw error
+    }
+  }
+  res.redirect(302, '/')
+})
 
 router.get('/login', (req, res) => {
   res.render('./guest/login');
+})
+
+router.post('/login', (req, res) => {
+
 })
 
 module.exports = router;
