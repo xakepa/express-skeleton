b4c9568c09a78896af248c882be08aec9cdad394
xakepa <plamen_ivanov@mail.com>
2020-06-24 18:27:57 +0300

    config changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../models/user')
 
 router.get('/register', (req, res) => {
   res.render('./guest/register');
 })
+
+// router.post('/register', (req, res) => {
+
+// })
 
 router.get('/login', (req, res) => {
   res.render('./guest/login');
